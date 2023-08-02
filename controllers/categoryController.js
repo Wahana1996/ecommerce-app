e367d6fb924044624561e1ec87fe83ad6fd9f5f3
Wahana1996 <116705047+Wahana1996@users.noreply.github.com>
2023-08-02 08:23:19 +0700

    final code
    ecommerce mern stack app

---
+++ controllers/categoryController.js
@@ -0,0 +1,121 @@
+import categoryModels from "../models/categoryModels.js";
+import slugify from "slugify";
+
+export const createCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(401).send({
+        message: "Name is required",
+      });
+    }
+    //check
+    const existingCategory = await categoryModels.findOne({ name });
+    //existing user
+    if (existingCategory) {
+      return res.status(200).send({
+        succes: true,
+        message: "Category Already Exist",
+      });
+    }
+    //save
+    const category = await new categoryModels({
+      name,
+      slug: slugify(name),
+    }).save();
+    return res.status(201).send({
+      success: true,
+      message: "New Category Created",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in Category",
+    });
+  }
+};
+
+//update category
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const category = await categoryModels.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Category Updated Succesfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While Update Category",
+    });
+  }
+};
+
+//get all category
+export const categoryController = async (req, res) => {
+  try {
+    const category = await categoryModels.find({});
+    res.status(200).send({
+      success: true,
+      message: "All Categories List",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting all categories",
+    });
+  }
+};
+
+//single category
+export const singleCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModels.findOne({ slug: req.params.slug });
+    res.status(200).send({
+      success: true,
+      message: "Get Single Category Successfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting single categorie",
+    });
+  }
+};
+
+//delete category
+export const deleteCategoryController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await categoryModels.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "Delete Categories Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "error while deleting category",
+    });
+  }
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import categoryRoute from "./routes/categoryRoute.js";
+import productRoute from "./routes/productRoute.js";
+import cors from "cors";
+import path from "path";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+// rest object
+const app = express();
+
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+//routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoute);
+
+// rest api
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+//run listen, WAHANA from .env
+app.listen(PORT, () => {
+  console.log(
+    `Server running on ${process.env.WAHANA} mode on port ${PORT}`.bgCyan.black
+  );
+});
